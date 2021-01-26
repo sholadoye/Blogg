023907757ce5a24885fb8ca8f29cbe6bc87f4525
sholadoye <shawlahrdoyet@gmail.com>
2021-01-27 00:01:21 +0100

    initial commit

---
+++ src/components/Main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Main = () => {
+    return (
+        <div className="page">
+            <img src="./images/anymag.png" alt="anymag"/>
+        </div>
+    )
+}
+
+export default Main
