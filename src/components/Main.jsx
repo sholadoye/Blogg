f2719c3646a5bb5fe09cc0da0c2b1b090ae18bc5
sholadoye <shawlahrdoyet@gmail.com>
2021-01-27 02:34:43 +0100

    commit

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -5,10 +5,31 @@ const Main = () => {
         <div className="page">
             <div className="anymag">
                 <img src="./images/anymag.png" alt="anymag"  className="any"/>
+                <p>Magazine Style WordPress Blog</p>
+                <button>
+                    Documentation
+                </button>
 
                 <img src="./images/magazine.png" alt="magazine" className="mag"/>
             </div>
-            
+
+            <span className="demo">
+                Demos
+            </span>
+
+            <div className="demo-pics">
+                <div className="demo-main">
+                    <img src="./images/demo-main.jpg" alt="main"/>
+                </div>
+
+                <div className="demo-dark">
+                    <img src="./images/demo-dark.jpg" alt="dark"
+/>
+                </div>
+            </div>
+
+
+
         </div>
     )
 }
